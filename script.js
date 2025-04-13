e44d202df12759a44b305edee1b3f8b7d61137ba
HoleInTheUniverse <43988974+HoleInTheUniverse@users.noreply.github.com>
2025-04-13 14:45:41 +0300

    Add basic logic for computer choice

---
+++ script.js
@@ -0,0 +1,16 @@
+function getComputerChoice () {
+    let choice = Math.floor(Math.random() * 100);
+
+    if (choice <= 32) {
+        return "rock";
+    }
+    else if ((choice > 32) && (choice <= 65)) {
+        return "paper";
+    }
+    else if ((choice > 65) && (choice <= 98)) {
+        return "scissors";
+    }
+    else {
+        return getComputerChoice();
+    }
+}
