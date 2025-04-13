c56293b4d74c16028b703065341b86abfdf572f3
HoleInTheUniverse <43988974+HoleInTheUniverse@users.noreply.github.com>
2025-04-13 15:06:14 +0300

    Add a function for one round play

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getComputerChoice () {
+function getComputerChoice() {
     let choice = Math.floor(Math.random() * 100);
 
     if (choice <= 32) {
@@ -15,6 +15,61 @@ function getComputerChoice () {
     }
 }
 
-function getHumanChoice () {
+function getHumanChoice() {
     return prompt("Enter your choice");
 }
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    
+    if (humanChoice === computerChoice) {
+        console.log("It's a draw!");
+        return;
+    }
+
+    if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            console.log("You lose! Paper beats Rock.");
+            computerScore++;
+            return;
+        }
+        else if (computerChoice === "scissors") {
+            console.log("You win! Rock beats Scissors");
+            humanScore++;
+            return;
+        }
+    }
+    else if (humanChoice === "paper") {
+        if (computerChoice === "scissors") {
+            console.log("You lose! Scissors beat Paper.");
+            computerScore++;
+            return;
+        }
+        else if (computerChoice === "rock") {
+            console.log("You win! Paper beats Rock");
+            humanScore++;
+            return;
+        }
+    }
+    else if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            console.log("You lose! Rock beats Scissors.");
+            computerScore++;
+            return;
+        }
+        else if (computerChoice === "paper") {
+            console.log("You win! Scissors beat Paper");
+            humanScore++;
+            return;
+        }
+    }
+    else {
+        console.log("Please enter correct value");
+        return;
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+playRound(getHumanChoice(), getComputerChoice());
